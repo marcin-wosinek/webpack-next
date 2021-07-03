57dfa8d220f79fdb2719b665409cd3b6cb39436a
Marcin Wosinek <marcin@wosinek.eu>
2021-07-03 13:03:47 +0200

    add custom loader

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: /\.my-file$/i,
+      loader: "raw-loader",
+    });
+
+    // Important: return the modified config
+    return config;
+  },
+};
